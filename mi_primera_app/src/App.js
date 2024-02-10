5cf59eb4e43c8b7cd1d5396d55654fc8c785b12e
Tecnologías <pc-15@iMac-de-Tecnologias.local>
2024-02-10 10:45:30 -0600

    primer commit

---
+++ mi_primera_app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import {MyComponent, Presentacion} from "./MyComponent"
+function App() {
+  return (
+    <div className="App">
+      <MyComponent />
+      <Presentacion />
+    </div>
+  );
+}
+
+export default App;
---
+++ mi_primera_app/src/MyComponent.js
@@ -0,0 +1,13 @@
+export function MyComponent()
+{
+   return <h1>Este es mi componente</h1>
+}
+
+export function Presentacion()
+{
+  return <div>
+  <h1> Mi nombre es Juan Ernesto Jurado Ledezma</h1>
+  <h3>Mi edad es 42 años</h3>
+  <p>Trabajo en Los Alamos</p>
+  </div>
+}
